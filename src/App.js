3a78cf89c9c632a4c6e0f50b37de4d06a1621337
QaraByte <pchelpkz16@gmail.com>
2022-10-02 22:36:16 +0600

    1) Убрал лишние файлы; 2) Переделал главн.страницу. Теперь две кнопки. Срабатывает инкремент.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // const [count,setCount]=useState(6)
+    // console.log(count);
+    // console.log(setCount);
+    // return ( <div className="App">
+
+    // </div>
+    let likes=0;
+
+    function Increment(){
+        likes+=1;
+        console.log(likes);
+    }
+
+    return (
+    <div className='app'>
+        <h3>{likes}</h3>
+        <button onClick={Increment}>Increment</button>
+        <button onClick={()=>likes-=1}>Decrement</button>
     </div>
-  );
+    );
 }
 
 export default App;
